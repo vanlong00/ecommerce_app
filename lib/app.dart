4cc2ee350dd16a6b358ca75ca77b17c630904288
Long Truong Van <intern.tvlong1@tma.com.vn>
2022-08-25 14:59:59 +0700

    Initial structure project

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
