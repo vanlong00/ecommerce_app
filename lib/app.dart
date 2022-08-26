aceebdc5cf592d862b4f314c4c7ca6bd6edad97c
vanlong00 <fly.ride777@gmail.com>
2022-08-26 16:21:08 +0700

    init auth bloc

--- lib/app.dart
+++ lib/app.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/auth/auth_bloc.dart';
+import 'data/repositories/auth_repository.dart';
+import 'presentation/SignIn/sign_in_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: BlocProvider(
+        create: (context) => AuthBloc(authRepository: RepositoryProvider.of<AuthRepository>(context)),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const SignInPage(),
+        ),
       ),
-      home: const AppView(),
     );
-  }
-}
-
-class AppView extends StatefulWidget {
-  const AppView({Key? key}) : super(key: key);
-
-  @override
-  State<AppView> createState() => _AppViewState();
-}
-
-class _AppViewState extends State<AppView> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
